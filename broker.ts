2f39eae0a0a6579a531d5fde439c001f2a148e69
jiwei <jiwei3778@bjsasc.com>
2017-04-14 20:18:26 +0800

    更改实现方式

--- broker.ts
+++ broker.ts
@@ -2,111 +2,108 @@
  * @Author: jiwei
  * @Date: 2017-04-10 10:06:26
  * @Last Modified by: jiwei
- * @Last Modified time: 2017-04-10 19:03:34
+ * @Last Modified time: 2017-04-14 19:50:04
  */
 
 //
 // ─── INTERFACE ──────────────────────────────────────────────────────────────────
 //
 interface configObj {
-    MQTTURL:string
+    readonly MQTTURL: string,
+    readonly RESTAPI?: string
+}
+interface connectopts {
+    username: string,
+    password: string,
+    token?: string
 }
 // ────────────────────────────────────────────────────────────────────────────────
-const config:configObj = {MQTTURL :"mqtt://test.mosquitto.org"};
+const config: configObj = { MQTTURL: "ws://10.0.32.106:8198" };
 const mqtt = require('mqtt');
-const events = require('events');
+const q = require("q");
 class Broker {
-    private brokerUrl:string;
-    private client:any={};
-    constructor(url:string=config.MQTTURL){
-        this.brokerUrl=url;
+    private brokerUrl: string;
+    constructor(url: string = config.MQTTURL) {
+        this.brokerUrl = url;
     }
-    public connect(token:string=""):any{
-        let url = this.brokerUrl+token;
-        this.client = mqtt.connect(url);
-    }
-    public connected():boolean{
-        return this.client.connected();
-    }
-    public getLastMessageId():string{
-        return this.client.getLastMessageId();
-    }
-    public reconnecting():boolean{
-        return this.client.reconnecting();
-    }
-    public subscribe(topic:any,opts=0):any{
-        let _this = this;
-        let promise =  new Promise((resolve:Function,reject:Function)=>{
-            if(_this.client){
-                _this.client.subscribe(topic,opts,(err:string,granted:any)=>{
-                    if(err) reject(err);
-                    resolve(granted);
-                });
-            }else{
-                reject("请先建立服务器链接");
-            }
-        })
-        return promise;
-    }
-    public unsubscribe(topic:any):any{
-        let _this = this;
-        let promise =  new Promise((resolve:Function,reject:Function)=>{
-            if(_this.client){
-                _this.client.unsubscribe(topic,(err:string)=>{
-                    if(err) reject(err);
-                    resolve("success");
-                });
-            }else{
-                reject("请先建立服务器链接");
-            }
-        })
-        return promise;
-    }
-    public end():any{
-        let _this =this;
-        return new Promise((resolve:Function,reject:Function)=>{
-            if(_this.client){
-               _this.client.end();
-            }
-        })
-    }
-    public on(event:string,fn:Function):any{
-        let _this =this;
-        if(_this.client){
-            _this.client.on(event,fn);
-        }else{
-            console.log("请先建立服务器链接");
-        }
-    }
-    public publish(topic:string,message:any,opts={qos:0,retain:false}):any{
-        let _this =this;
-        return new Promise((resolve:Function,reject:Function)=>{
-            if(_this.client){
-                _this.client.publish(topic,message,(err:any)=>{
-                    if(err) reject(err)
-                    else resolve("success");
-                });
-            }else{
-                console.log("请先建立服务器链接");
-            }
-        })
-    }
-    public publishToAsp(message:string,opts={qos:0,retain:false}):any{
-        return this.publish("messageSendToAsp",message,opts);
-    }
-    public publishToClient(topic:string,message:any,opts={qos:0,retain:false}):any{
-        return this.publish(topic,message,opts);
+    public connect({ token = "", username, password }: connectopts): any {
+        let url = this.brokerUrl + token;
+        let mqttclient = mqtt.connect(url, {
+            username: username,
+            password: password,
+            protocolId: 'MQIsdp',
+            protocolVersion: 3
+        });
+        return new Client(mqttclient);
     }
 }
-
+class Client {
+    private AsyncClient: any;
+    constructor(client: any) {
+        this.AsyncClient = client;
+    }
+    public publish(topic: string, message: any, opts = { qos: 0, retain: false }): any {
+        let deferred = q.defer();
+        this.AsyncClient.publish(topic, message,opts, (err: any) => {
+            if (err) deferred.reject(err)
+            else deferred.resolve("success");
+        });
+        return deferred.promise;
+    }
+    public publishToAsp(message: string, opts = { qos: 0, retain: false }): any {
+        return this.publish("messageSendToAsp", message, opts);
+    }
+    public publishToClient(topic: string, message: any, opts = { qos: 0, retain: false }): any {
+        return this.publish(topic, message, opts);
+    }
+    public on() {
+        return this.AsyncClient.on.apply(this.AsyncClient, arguments);
+    }
+    public end() {
+        return this.AsyncClient.end.apply(this.AsyncClient, arguments);
+    }
+    public subscribe(topic: any, opts = 0): any {
+        let deferred = q.defer();
+        this.AsyncClient.subscribe(topic, opts, (err: string, granted: any) => {
+            if (err) deferred.reject(err);
+            deferred.resolve(granted);
+        })
+        return deferred.promise;
+    }
+    public unsubscribe(topic: any): any {
+        let deferred = q.defer();
+        this.AsyncClient.unsubscribe(topic, (err: string) => {
+            if (err) deferred.reject(err);
+            deferred.resolve("success");
+        });
+        return deferred.promise;
+    }
+    public connected() {
+        return this.AsyncClient.connected.apply(this.AsyncClient, arguments);
+    }
+    public reconnecting() {
+        return this.AsyncClient.reconnecting.apply(this.AsyncClient, arguments);
+    }
+    public getLastMessageId() {
+        return this.AsyncClient.getLastMessageId.apply(this.AsyncClient, arguments);
+    }
+}
 let bbroker = new Broker();
-bbroker.connect();
-bbroker.on('connect', function () {
-  bbroker.subscribe('presence').then(()=>{
-    return bbroker.publish('presence', 'Hello mqtt')
-  }).then(()=>{
-    bbroker.on('message', function (topic:any, message:any) {
-        console.log(message.toString())
-    })
-  })
+var a = bbroker.connect({
+    username: "admin",
+    password: "bjsasc"
+});
+console.log(a.on)
+a.on("connect",function(){
+    a.subscribe('presence').then((res)=>{
+        console.log(res)
+        a.publish('presence', 'Hello mqtt').then((res)=>{
+            console.log(res)
+        });
+    });
+})
+a.on('message', function (topic:any, message:any) {
+  // message is Buffer
+  console.log(message.toString())
+  a.end()
 })
